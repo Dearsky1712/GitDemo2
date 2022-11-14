e5997e7f6309f03a2f8fc6aae53ba32e5e5855de
Dearsky1712 <dearsky1712@gmail.com>
2022-11-14 20:00:23 +0530

    first demo2 commit

---
+++ GitDemo2/src/com/git/Demo2/TestHashMap.java
@@ -0,0 +1,16 @@
+package com.git.Demo2;
+
+import java.util.HashMap;
+
+public class TestHashMap {
+
+	public static void main(String[] args) {
+		HashMap hs = new HashMap();
+       hs.put("Aakash","Bawale");
+       hs.put("Gunjan", "Patil");
+       hs.put("Komal","Pandey");
+       hs.put("Gokul", "Patil");
+       System.out.println(hs);
+	}
+
+}
